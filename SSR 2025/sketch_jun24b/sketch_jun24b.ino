fb30ef775d9bee15ebe6441fa776b8592ba49286
Susanna Boberg <sboberg617@gmail.com>
2025-06-24 16:28:50 -0400

    this was painful

--- SSR 2025/sketch_jun24b/sketch_jun24b.ino
+++ SSR 2025/sketch_jun24b/sketch_jun24b.ino
@@ -1,133 +1,102 @@
-
 #include "Adafruit_seesaw.h"
 #include <seesaw_neopixel.h>
-
 
 #include <SPI.h>
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 //OLED display height
+#include <Servo.h>
 
-#define SS_SWITCH        24
-#define SS_NEOPIX        6
+// Potentiometer Info
+#define SS_SWITCH 24
+#define SS_NEOPIX 6
+#define SEESAW_ADDR 0x36
 
-#define SEESAW_ADDR          0x36
-
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3D ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+#define SERVO_PIN 9
 
 Adafruit_seesaw ss;
 seesaw_NeoPixel sspixel = seesaw_NeoPixel(1, SS_NEOPIX, NEO_GRB + NEO_KHZ800);
 
+Servo myservo;
+
 int32_t encoder_position;
-int switchPin = 2;
-int switchState;
-int prevSwitchState;
+int yRotationScale = 90;
 
-void setup() {
+void setup()
+{
   Serial.begin(115200);
-  pinMode(switchPin, INPUT);
+  myservo.attach(SERVO_PIN);
 
-  while (!Serial) delay(10);
 
-  if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    Serial.println(F("SSD1306 allocation failed"));
-    for (;;); // Don't proceed, loop forever
-  }
-  display.display();
+  while (!Serial)
+    delay(10);
 
-  if (! ss.begin(SEESAW_ADDR) || ! sspixel.begin(SEESAW_ADDR)) {
-    Serial.println("Couldn't find seesaw on default address");
-    while (1) delay(10);
-  }
-  // Serial.println("seesaw started");
+  /*
+    if (!ss.begin(SEESAW_ADDR) || !sspixel.begin(SEESAW_ADDR))
+    {
+      Serial.println("Couldn't find seesaw on default address");
+      while (1)
+        delay(10);
+    }
 
-  uint32_t version = ((ss.getVersion() >> 16) & 0xFFFF);
-  if (version  != 4991) {
-    //Serial.print("Wrong firmware loaded? ");
-    //Serial.println(version);
-    while (1) delay(10);
-  }
 
-  pinMode(LED_BUILTIN, OUTPUT);
-  // use a pin for the built in encoder switch
-  ss.pinMode(SS_SWITCH, INPUT_PULLUP);
+    uint32_t version = ((ss.getVersion() >> 16) & 0xFFFF);
+    if (version != 4991)
+    {
+      // Serial.print("Wrong firmware loaded? ");
+      // Serial.println(version);
+      while (1)
+        delay(10);
+    }
 
-  // get starting position
-  encoder_position = ss.getEncoderPosition();
+    pinMode(LED_BUILTIN, OUTPUT);
+    // use a pin for the built in encoder switch
+    ss.pinMode(SS_SWITCH, INPUT_PULLUP);
+    // get starting position
+    encoder_position = ss.getEncoderPosition();
 
-  // Serial.println("Turning on interrupts");
-  delay(10);
-  ss.setGPIOInterrupts((uint32_t)1 << SS_SWITCH, 1);
-  ss.enableEncoderInterrupt();
+    // Serial.println("Turning on interrupts");
+    delay(10);
+    ss.setGPIOInterrupts((uint32_t)1 << SS_SWITCH, 1);
+    ss.enableEncoderInterrupt();
+  */
 
-  delay(100);
-  Serial.flush();
 }
-
-int xScale;
-int yScale;
 int frame;
 bool readAnything;
-float unityFloatX = 0.5;
-float unityFloatY = 0.5;
+float unityRotationY;
 
-void loop() {
-  int32_t new_position = ss.getEncoderPosition();
-
-  //ROTATION OF KNOB FOR SIZE OF OBJECT IN UNITY
-  if (new_position > 50) {
-    ss.setEncoderPosition(50);
-    new_position = 50;
-  }
-  if (new_position < -50) {
-    ss.setEncoderPosition(-50);
-    new_position = -50;
-  }
-
-  if (new_position != encoder_position) {
-    Serial.println(inverseLerp(-50.0, 50.0, new_position)); // FORNOW (clamp is unnecessary)
-    encoder_position = new_position;      // and save for next round
-  }
-
-  
-  prevSwitchState = switchState;
-  switchState = digitalRead(switchPin);
-  if (switchState != prevSwitchState) {
-    if (switchState == HIGH) {
-      Serial.println("sTrue");
-    } else if (switchState == LOW) {
-      Serial.println("sFalse");
-    }
-  }
-
+void loop()
+{
+  //int32_t new_position = ss.getEncoderPosition();
   readSerial();
-  xScale = round(unityFloatX * 128);
-  yScale = round(unityFloatY * 64);
 
-  if (readAnything && (frame++ % 30 == 0)) {
-    display.clearDisplay();
-    display.drawPixel(xScale, yScale, SSD1306_WHITE);
-    display.display();
+  // update encoder position & LED after reading unity rotation
+  if (readAnything && (frame % 30 == 0))
+  {
+
+    yRotationScale = round(unityRotationY * 180);
+    myservo.write(yRotationScale);
+    //myservo.write(sin(frame / 30.0) * 90 + 90);
   }
+
+
+   frame++;
 }
 
-void readSerial() {
-  if (Serial.available()) {
+
+void readSerial()
+{
+  if (Serial.available())
+  {
     String unityData = Serial.readStringUntil('\n');
-    if (unityData[0] == 'x') {
-      unityFloatX = unityData.substring(1).toFloat();
-    } else {
-      unityFloatY = unityData.substring(1).toFloat();
-    }
+    unityRotationY = unityData.toFloat();
     readAnything = true;
   }
 }
 
-float inverseLerp(float l, float u, int32_t pos) {
+float inverseLerp(float l, float u, int32_t pos)
+{
   return ((float)pos - l) / (u - l);
 }
--- VR Test/Assets/Scripts/encoderController.cs
+++ VR Test/Assets/Scripts/encoderController.cs
@@ -1,17 +1,46 @@
 using UnityEngine;
-
+using System.IO.Ports;
+using System.Collections; 
 public class encoderController : MonoBehaviour
 {
-
-    float currPos;
+    
+    SerialPort sp;
+    int frame;
     void Start()
     {
-        currPos = transform.rotation.y;
+        sp = new SerialPort("/dev/tty.usbserial-110", 115200);
+        sp.Open();
+        frame = 0;
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.R)) { transform.Rotate(0f, 0.25f, 0f); }
-        if (Input.GetKey(KeyCode.L)) { transform.Rotate(0f, -0.25f, 0f); }
+        if (frame++ % 8 == 0) 
+        {
+            if (Input.GetKey(KeyCode.R)) { print("R Pressed");  rotate('y', 1); }
+            if (Input.GetKey(KeyCode.L)) { print("L Pressed");  rotate('y', -1); }
+        }
+    }
+
+    void rotate(char axis, int direction)
+    /*rotates object and sends information to arduino. axis is defined 
+    by x, y, or z, direction by -1 or 1 (backward or forward)*/
+    {
+        float x = 0f, y = 0f, z = 0f;
+        if (axis == 'x') { x = 1f * direction; }
+        else if (axis == 'y') { y = 1f * direction; }
+        else if (axis == 'z') { z = 1f * direction; }
+        transform.Rotate(x, y, z);
+        float unitRotation = 0f;
+
+        if (axis == 'x') { unitRotation = inverseLerp(transform.rotation.x, -1f, 1f); }
+        else if (axis == 'y') { unitRotation = inverseLerp(transform.rotation.y, -1f, 1f); }
+        else if (axis == 'z') { unitRotation = inverseLerp(transform.rotation.z, -1f, 1f); }
+        sp.WriteLine(unitRotation.ToString());
+    }
+
+    float inverseLerp(float currRotation, float lower, float upper)
+    {
+        return (currRotation - lower) / (upper - lower);
     }
 }
